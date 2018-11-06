d314105b1583ed223194b0f447ac6ded4ffb2aa0
Eike Foken <foken@cybay.de>
2018-11-06 15:36:16 +0100

    Data optimizations

--- src/App.vue
+++ src/App.vue
@@ -89,7 +89,13 @@
           </h1>
           <div class="subheading mb-5">
             Vahrenwalder Str. 115 · Hanover, DE 20165 · +49 1578 5916655 ·
-            <a href="mailto:kontakt@eikefoken.de">kontakt@eikefoken.de</a>
+            <a
+              :style="{ unicodeBidi: 'bidi-override', direction: 'rtl' }"
+              href="#"
+              @click="contactMe"
+            >
+              ed.nekofekie@tkatnok
+            </a>
           </div>
           <p class="lead mb-5">
             I am Eike Foken, a Web and Frontend Developer with 7 years of experience with modern web
@@ -100,6 +106,8 @@
           <div class="social-icons">
             <a
               href="https://www.xing.com/profile/Eike_Foken"
+              target="_blank"
+              rel="noopener noreferrer"
               aria-label="Xing"
             >
               <i
@@ -109,6 +117,8 @@
             </a>
             <a
               href="https://github.com/efoken"
+              target="_blank"
+              rel="noopener noreferrer"
               aria-label="GitHub"
             >
               <i
@@ -118,6 +128,8 @@
             </a>
             <a
               href="https://www.facebook.com/eike.foken"
+              target="_blank"
+              rel="noopener noreferrer"
               aria-label="Facebook"
             >
               <i
@@ -138,75 +150,21 @@
         <div class="my-auto">
           <h2 class="mb-5">Experience</h2>
 
-          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+          <div
+            v-for="(experience, index) in experiences"
+            :key="index"
+            :class="{ 'mb-5': index !== experiences.length - 1 }"
+            class="resume-item d-flex flex-column flex-md-row"
+          >
             <div class="resume-content mr-auto">
-              <h3 class="mb-0">Senior Web Developer</h3>
-              <div class="subheading mb-3">Cybay New Media GmbH, Hanover</div>
-              <p>
-                In the agency I am dealing with the creation of websites, further development of
-                existing websites, as well as the care of existing customers. In addition to PHP,
-                Laravel and WordPress, the technologies used include JavaScript, React for the
-                development of Progressive Web Apps and React Native for mobile app development.
-              </p>
+              <h3 class="mb-0">{{ experience.title }}</h3>
+              <div class="subheading mb-3">{{ experience.company }}</div>
+              <p>{{ experience.description }}</p>
             </div>
             <div class="resume-date text-md-right">
-              <span class="text-primary">January 2018 - Present</span>
-            </div>
-          </div>
-
-          <div class="resume-item d-flex flex-column flex-md-row mb-5">
-            <div class="resume-content mr-auto">
-              <h3 class="mb-0">Software Developer</h3>
-              <div class="subheading mb-3">ABIAN GmbH, Hanover</div>
-              <p>
-                Originally hired as a developer for web-based software (e.g. for the Deutsche Messe
-                AG and CEBIT), over time I took over more and more tasks and was responsible for the
-                implementation of responsive websites and mobile apps, from conception to
-                development towards implementation with PHP, JavaScript and CSS with well known
-                frameworks like Laravel, Angular, React and Bootstrap.
-              </p>
-            </div>
-            <div class="resume-date text-md-right">
-              <span class="text-primary">January 2014 - December 2017</span>
-            </div>
-          </div>
-
-          <div class="resume-item d-flex flex-column flex-md-row mb-5">
-            <div class="resume-content mr-auto">
-              <h3 class="mb-0">Junior Web Developer</h3>
-              <div class="subheading mb-3">eMedienservice Nord, Oldenburg</div>
-              <p>
-                An agency, a subsidiary of the Nordwest-Zeitung, from which I was taken over after
-                working at QP media GmbH. In addition to the independent development of responsive
-                websites and WordPress theming, my activities also included the programming of
-                <a
-                  href="https://www.nwzonline.de/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  www.nwzonline.de
-                </a>
-                with Java.
-              </p>
-            </div>
-            <div class="resume-date text-md-right">
-              <span class="text-primary">August 2013 - December 2013</span>
-            </div>
-          </div>
-
-          <div class="resume-item d-flex flex-column flex-md-row">
-            <div class="resume-content mr-auto">
-              <h3 class="mb-0">Junior Web Developer</h3>
-              <div class="subheading mb-3">QP media GmbH, Oldenburg</div>
-              <p>
-                A small startup, sponsored by Nordwest-Zeitung, which dealt with couponing and
-                printing vouchers. From the first day on I accompanied the startup, developed a
-                well-known, regional coupon platform. Programming languages used included Python
-                (with Django) and JavaScript.
-              </p>
-            </div>
-            <div class="resume-date text-md-right">
-              <span class="text-primary">October 2011 - July 2013</span>
+              <span class="text-primary">
+                {{ experience.startDate }} - {{ experience.endDate }}
+              </span>
             </div>
           </div>
 
@@ -223,15 +181,22 @@
         <div class="my-auto">
           <h2 class="mb-5">Education</h2>
 
-          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+          <div
+            v-for="(education, index) in educations"
+            :key="index"
+            :class="{ 'mb-5': index !== experiences.length - 1 }"
+            class="resume-item d-flex flex-column flex-md-row"
+          >
             <div class="resume-content mr-auto">
-              <h3 class="mb-0">Hochschule Emden/Leer</h3>
-              <div class="subheading mb-3">Bachelor of Science</div>
-              <div>Computer Science - Web Development Track</div>
-              <p>GPA: 2.31</p>
+              <h3 class="mb-0">{{ education.title }}</h3>
+              <div class="subheading mb-3">{{ education.graduation }}</div>
+              <div>{{ education.subject }}</div>
+              <p>GPA: {{ education.gpa }}</p>
             </div>
             <div class="resume-date text-md-right">
-              <span class="text-primary">September 2008 - March 2012</span>
+              <span class="text-primary">
+                {{ education.startDate }} - {{ education.endDate }}
+              </span>
             </div>
           </div>
 
@@ -249,174 +214,34 @@
 
           <div class="subheading mb-3">Programming Languages &amp; Tools</div>
           <ul class="list-inline dev-icons">
-            <li class="list-inline-item">
+            <li
+              v-for="tool in skills.tools"
+              :key="tool.icon"
+              class="list-inline-item"
+            >
               <i
-                class="devicon-angularjs-plain"
-                aria-label="Angular"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-amazonwebservices-original"
-                aria-label="Amazon Web Services"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-atom-original"
-                aria-label="Atom"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-babel-plain"
-                aria-label="Babel"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-bootstrap-plain"
-                aria-label="Bootstrap"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-css3-plain"
-                aria-label="CSS3"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-django-plain"
-                aria-label="Django"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-docker-plain"
-                aria-label="Docker"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-express-original"
-                aria-label="Express"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-html5-plain"
-                aria-label="HTML5"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-git-plain"
-                aria-label="Git"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-gulp-plain"
-                aria-label="gulp"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-javascript-plain"
-                aria-label="JavaScript"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-laravel-plain"
-                aria-label="Laravel"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-nginx-original"
-                aria-label="Nginx"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-nodejs-plain"
-                aria-label="Node.js"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-php-plain"
-                aria-label="PHP"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-postgresql-plain"
-                aria-label="PostgreSQL"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-python-plain"
-                aria-label="Python"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-react-original"
-                aria-label="React"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-redis-plain"
-                aria-label="Redis"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-sass-original"
-                aria-label="Sass"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-vuejs-plain"
-                aria-label="Vue.js"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-webpack-plain"
-                aria-label="webpack"
-              />
-            </li>
-            <li class="list-inline-item">
-              <i
-                class="devicon-wordpress-plain"
-                aria-label="WordPress"
+                :class="tool.icon"
+                :aria-label="tool.title"
               />
             </li>
           </ul>
 
           <div class="subheading mb-3">Workflow</div>
-          <ul class="fa-ul mb-0 list-unstyled">
+          <ul class="list-icons">
             <li>
-              <i class="fa-li ion ion-md-checkmark" />
+              <i class="ion ion-md-checkmark" />
               Mobile-First, Responsive Design
             </li>
             <li>
-              <i class="fa-li ion ion-md-checkmark" />
+              <i class="ion ion-md-checkmark" />
               Cross Browser Testing &amp; Debugging
             </li>
             <li>
-              <i class="fa-li ion ion-md-checkmark" />
+              <i class="ion ion-md-checkmark" />
               Cross Functional Teams
             </li>
             <li>
-              <i class="fa-li ion ion-md-checkmark" />
+              <i class="ion ion-md-checkmark" />
               Agile Development, Scrum &amp; Kanban
             </li>
           </ul>
@@ -449,7 +274,84 @@
 
 <script>
 export default {
+  data() {
+    return {
+      experiences: [
+        {
+          title: 'Senior Web Developer',
+          company: 'Cybay New Media GmbH, Hanover',
+          description: 'In the agency I am dealing with the creation of websites, further development of existing websites, as well as the care of existing customers. In addition to PHP, Laravel and WordPress, the technologies used include JavaScript, React for the development of Progressive Web Apps and React Native for mobile app development.',
+          startDate: 'January 2018',
+          endDate: 'Present',
+        },
+        {
+          title: 'Software Developer',
+          company: 'ABIAN GmbH, Hanover',
+          description: 'Originally hired as a developer for web-based software (e.g. for the Deutsche Messe AG and CEBIT), over time I took over more and more tasks and was responsible for the implementation of responsive websites and mobile apps, from conception to development towards implementation with PHP, JavaScript and CSS with well known frameworks like Laravel, Angular, React and Bootstrap.',
+          startDate: 'January 2014',
+          endDate: 'December 2017',
+        },
+        {
+          title: 'Junior Web Developer',
+          company: 'eMedienservice Nord, Oldenburg',
+          description: 'An agency, a subsidiary of the Nordwest-Zeitung, from which I was taken over after working at QP media GmbH. In addition to the independent development of responsive websites and WordPress theming, my activities also included the programming of www.nwzonline.de with Java.',
+          startDate: 'August 2013',
+          endDate: 'December 2013',
+        },
+        {
+          title: 'Junior Web Developer',
+          company: 'QP media GmbH, Oldenburg',
+          description: 'A small startup, sponsored by Nordwest-Zeitung, which dealt with couponing and printing vouchers. From the first day on I accompanied the startup, developed a well-known, regional coupon platform. Programming languages used included Python (with Django) and JavaScript.',
+          startDate: 'October 2011',
+          endDate: 'July 2013',
+        },
+      ],
+      educations: [
+        {
+          title: 'Hochschule Emden/Leer',
+          graduation: 'Bachelor of Science',
+          subject: 'Computer Science - Web Development Track',
+          gpa: 2.31,
+          startDate: 'September 2008',
+          endDate: 'March 2012',
+        },
+      ],
+      skills: {
+        tools: [
+          { title: 'Amazon Web Services', icon: 'devicon-amazonwebservices-original' },
+          { title: 'Angular', icon: 'devicon-angularjs-plain' },
+          { title: 'Atom', icon: 'devicon-atom-original' },
+          { title: 'Babel', icon: 'devicon-babel-plain' },
+          { title: 'Bootstrap', icon: 'devicon-bootstrap-plain' },
+          { title: 'CSS3', icon: 'devicon-css3-plain' },
+          { title: 'Django', icon: 'devicon-django-plain' },
+          { title: 'Docker', icon: 'devicon-docker-plain' },
+          { title: 'Express', icon: 'devicon-express-original' },
+          { title: 'Git', icon: 'devicon-git-plain' },
+          { title: 'gulp', icon: 'devicon-gulp-plain' },
+          { title: 'HTML5', icon: 'devicon-html5-plain' },
+          { title: 'JavaScript', icon: 'devicon-javascript-plain' },
+          { title: 'Laravel', icon: 'devicon-laravel-plain' },
+          { title: 'Nginx', icon: 'devicon-nginx-original' },
+          { title: 'Node.js', icon: 'devicon-nodejs-plain' },
+          { title: 'PHP', icon: 'devicon-php-plain' },
+          { title: 'PostgreSQL', icon: 'devicon-postgresql-plain' },
+          { title: 'Python', icon: 'devicon-python-plain' },
+          { title: 'React', icon: 'devicon-react-original' },
+          { title: 'Redis', icon: 'devicon-redis-plain' },
+          { title: 'Sass', icon: 'devicon-sass-original' },
+          { title: 'Vue.js', icon: 'devicon-vuejs-plain' },
+          { title: 'webpack', icon: 'devicon-webpack-plain' },
+          { title: 'WordPress', icon: 'devicon-wordpress-plain' },
+        ],
+      },
+    };
+  },
   methods: {
+    contactMe(ev) {
+      ev.preventDefault();
+      window.location.href = atob('bWFpbHRvOmtvbnRha3RAZWlrZWZva2VuLmRl');
+    },
     scrollToAnchor(ev) {
       const href = ev.currentTarget.getAttribute('href');
       const target = href && href !== '#' ? document.querySelector(href) : null;
