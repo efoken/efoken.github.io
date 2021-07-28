2a9c2a6681b8482bf9952b6a41eda4d8e17f0726
Eike Foken <eike.foken@aperto.com>
2021-07-28 10:08:37 +0200

    Improve SSR app and navigation handling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { createTheme } from '@material-ui/core/styles';
 import createPalette from '@material-ui/core/styles/createPalette';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const palette = createPalette({
   primary: {
@@ -15,105 +15,119 @@ const palette = createPalette({
   },
 });
 
-const App: React.VFC<AppProps> = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <title>Eike Foken</title>
-    </Head>
-    <ThemeProvider
-      theme={createTheme({
-        palette,
-        typography: {
-          fontWeightBold: 800,
-          allVariants: {
-            fontFamily: 'Muli, sans-serif',
-          },
-          h1: {
-            fontFamily: '"Saira Extra Condensed", sans-serif',
-            fontSize: 96,
-            fontWeight: 700,
-            lineHeight: 5.5 / 6,
-            textTransform: 'uppercase',
-          },
-          h2: {
-            fontFamily: '"Saira Extra Condensed", sans-serif',
-            fontSize: 56,
-            fontWeight: 700,
-            lineHeight: 1.2,
-            textTransform: 'uppercase',
-          },
-          h3: {
-            fontFamily: '"Saira Extra Condensed", sans-serif',
-            fontSize: 32,
-            fontWeight: 700,
-            lineHeight: 1.2,
-            textTransform: 'uppercase',
-          },
-          h4: {
-            fontFamily: '"Saira Extra Condensed", sans-serif',
-            fontSize: 24,
-            fontWeight: 500,
-            lineHeight: 1.5,
-            textTransform: 'uppercase',
-          },
-          body1: {
-            fontSize: 18,
-            letterSpacing: 'normal',
-            lineHeight: 1.5,
-          },
-          body2: {
-            fontSize: 16,
-            letterSpacing: 'normal',
-            lineHeight: 1.5,
-          },
-          button: {
-            fontSize: 16,
-            fontWeight: 800,
-            letterSpacing: 0.8,
-            lineHeight: 1.5,
-            textTransform: 'uppercase',
-          },
-        },
-        overrides: {
-          MuiAvatar: {
-            circular: {
-              borderColor: 'rgba(255, 255, 255, 0.2)',
-              borderStyle: 'solid',
-              borderWidth: 8,
-              height: 160,
-              width: 160,
+const App: React.VFC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    jssStyles?.parentElement?.removeChild(jssStyles);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Eike Foken</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider
+        theme={createTheme({
+          palette,
+          typography: {
+            fontWeightBold: 800,
+            allVariants: {
+              fontFamily: 'Muli, sans-serif',
+            },
+            h1: {
+              fontFamily: '"Saira Extra Condensed", sans-serif',
+              fontSize: 96,
+              fontWeight: 700,
+              lineHeight: 88 / 96,
+              textTransform: 'uppercase',
+            },
+            h2: {
+              fontFamily: '"Saira Extra Condensed", sans-serif',
+              fontSize: 56,
+              fontWeight: 700,
+              lineHeight: 72 / 56,
+              textTransform: 'uppercase',
+            },
+            h3: {
+              fontFamily: '"Saira Extra Condensed", sans-serif',
+              fontSize: 32,
+              fontWeight: 700,
+              lineHeight: 40 / 32,
+              textTransform: 'uppercase',
+            },
+            h4: {
+              fontFamily: '"Saira Extra Condensed", sans-serif',
+              fontSize: 24,
+              fontWeight: 500,
+              lineHeight: 36 / 24,
+              textTransform: 'uppercase',
+            },
+            body1: {
+              fontSize: 18,
+              letterSpacing: 'normal',
+              lineHeight: 24 / 18,
+            },
+            body2: {
+              fontSize: 16,
+              letterSpacing: 'normal',
+              lineHeight: 1.5,
+            },
+            button: {
+              fontSize: 16,
+              fontWeight: 800,
+              letterSpacing: 0.8,
+              lineHeight: 1.5,
+              textTransform: 'uppercase',
             },
           },
-          MuiButton: {
-            text: {
-              color: 'rgba(255, 255, 255, 0.65)',
-              padding: 8,
+          overrides: {
+            MuiAvatar: {
+              circular: {
+                borderColor: 'rgba(255, 255, 255, 0.2)',
+                borderStyle: 'solid',
+                borderWidth: 8,
+                height: 160,
+                width: 160,
+              },
+            },
+            MuiButton: {
+              text: {
+                color: 'rgba(255, 255, 255, 0.65)',
+                padding: 8,
 
-              '&.active': {
-                color: '#fff',
+                '&.active': {
+                  color: '#fff',
+                },
+              },
+            },
+            MuiDrawer: {
+              root: {
+                width: 272,
+              },
+              paper: {
+                backgroundColor: palette.primary.main,
+              },
+            },
+            MuiTypography: {
+              gutterBottom: {
+                marginBottom: 8,
               },
             },
           },
-          MuiDrawer: {
-            root: {
-              width: 272,
-            },
-            paper: {
-              backgroundColor: palette.primary.main,
+          props: {
+            MuiTypography: {
+              variant: 'body2',
             },
           },
-        },
-        props: {
-          MuiTypography: {
-            variant: 'body2',
-          },
-        },
-      })}
-    >
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  </>
-);
+        })}
+      >
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,72 +15,71 @@ if (typeof window !== 'undefined') {
 
 function handleScrollToAnchor(ev: React.MouseEvent<HTMLElement>) {
   const href = ev.currentTarget.getAttribute('href');
-  const target = href && href !== '#' ? document.querySelector(href) : null;
-  if (target) {
+  const target = href?.startsWith('#') ? document.querySelector(href) : null;
+
+  if (target != null) {
     target.scrollIntoView({ behavior: 'smooth' });
     ev.preventDefault();
-  } else if (href === '#') {
+  } else if (href === '/') {
     window.scroll({ top: 0, left: 0, behavior: 'smooth' });
     ev.preventDefault();
   }
 }
 
-const Home: NextPage = () => {
-  return (
-    <Box display="flex">
-      <Hidden smDown>
-        <Drawer variant="permanent" PaperProps={{ style: { width: 272 } }}>
-          <Box mt="auto" mx="auto">
-            <Link href="/" onClick={handleScrollToAnchor}>
-              <Avatar
-                alt="Eike Foken"
-                src="/images/profile.jpg"
-                srcSet="/images/profile@2x.jpg 2x, /images/profile@3x.jpg 3x"
-              />
-            </Link>
-          </Box>
-          <Box component="nav" mt={2} mb="auto">
-            <Scrollspy
-              items={['about', 'experience', 'education', 'skills', 'interests']}
-              componentTag={({ children }) => (
-                <Box display="flex" flexDirection="column">
-                  {children}
-                </Box>
-              )}
-              currentClassName="active"
-            >
-              <Button href="#about" onClick={handleScrollToAnchor}>
-                About
-              </Button>
-              <Button href="#experience" onClick={handleScrollToAnchor}>
-                Experience
-              </Button>
-              <Button href="#education" onClick={handleScrollToAnchor}>
-                Education
-              </Button>
-              <Button href="#skills" onClick={handleScrollToAnchor}>
-                Skills
-              </Button>
-              <Button href="#interests" onClick={handleScrollToAnchor}>
-                Interests
-              </Button>
-            </Scrollspy>
-          </Box>
-        </Drawer>
-      </Hidden>
-      <main>
-        <About />
-        <Divider />
-        <Experience />
-        <Divider />
-        <Education />
-        <Divider />
-        <Skills />
-        <Divider />
-        <Interests />
-      </main>
-    </Box>
-  );
-};
+const Home: NextPage = () => (
+  <Box display="flex">
+    <Hidden smDown>
+      <Drawer variant="permanent" PaperProps={{ style: { width: 272 } }}>
+        <Box mt="auto" mx="auto">
+          <Link href="/" onClick={handleScrollToAnchor}>
+            <Avatar
+              alt="Eike Foken"
+              src="/images/profile.jpg"
+              srcSet="/images/profile@2x.jpg 2x, /images/profile@3x.jpg 3x"
+            />
+          </Link>
+        </Box>
+        <Box component="nav" mt={2} mb="auto">
+          <Scrollspy
+            items={['about', 'experience', 'education', 'skills', 'interests']}
+            componentTag={({ children }) => (
+              <Box display="flex" flexDirection="column">
+                {children}
+              </Box>
+            )}
+            currentClassName="active"
+          >
+            <Button href="#about" onClick={handleScrollToAnchor}>
+              About
+            </Button>
+            <Button href="#experience" onClick={handleScrollToAnchor}>
+              Experience
+            </Button>
+            <Button href="#education" onClick={handleScrollToAnchor}>
+              Education
+            </Button>
+            <Button href="#skills" onClick={handleScrollToAnchor}>
+              Skills
+            </Button>
+            <Button href="#interests" onClick={handleScrollToAnchor}>
+              Interests
+            </Button>
+          </Scrollspy>
+        </Box>
+      </Drawer>
+    </Hidden>
+    <main>
+      <About />
+      <Divider />
+      <Experience />
+      <Divider />
+      <Education />
+      <Divider />
+      <Skills />
+      <Divider />
+      <Interests />
+    </main>
+  </Box>
+);
 
 export default Home;
