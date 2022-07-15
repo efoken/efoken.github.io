b957d5999e5da505d899e49a7fe265c2c8534325
Eike Foken <eike.foken@aperto.com>
2022-07-15 18:32:53 +0200

    Add out directory to .gitignore

--- out/_next/static/jLcoVyUQ0vxIrmrSNz3A2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b,c,d){return {"/":[a,b,c,d],"/index":[a,b,c,d]}}("static\u002Fchunks\u002F0f1ac474.0c55fbc35dac86cc0457.js","static\u002Fchunks\u002F70b165ca.82a2c7751040b1de6f7f.js","static\u002Fchunks\u002F1bfc9850.c3f95f14704593bdc785.js","static\u002Fchunks\u002F95b64a6e.773cc5b80477d8514ede.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
